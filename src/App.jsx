857854f2828f41416b87bbfd47d47ab7b8955b58
Shin Thant Maung <shinthantmg224@gmail.com>
2023-07-11 15:38:35 +0630

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -159,7 +159,7 @@ function App() {
           {yearError && <p className="error-message">Must be in the past</p>}
         </form>
         <button onClick={ageStored ? restartor : clickHandler}>
-          <img src="./assets/images/icon-arrow.svg" alt="" />
+          <img src="/images/icon-arrow.svg" alt="" />
         </button>
       </div>
       <h2 className="answer"><span>{ageStored ? age.year : "- -"}</span> years</h2>
